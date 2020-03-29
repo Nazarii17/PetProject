2701bc00c12c0f8d4f6b793eece6979ce090e552
Nazarii Tkachuk <nazar.tkachuk.28@gmail.com>
2020-03-30 02:08:04 +0300

    added/updated constants

---
+++ src/main/java/com/tkachuk/pet/util/constants/Notifications.java
@@ -0,0 +1,24 @@
+package com.tkachuk.pet.util.constants;
+
+public enum Notifications {
+    NAME_UPDATED("Dear customer, your name was successfully changed!"),
+    PROFILE_PHOTO_UPDATED("Dear customer, your profile photo was successfully changed!"),
+    PASSWORD_UPDATED("Dear customer, your password was successfully changed!"),
+    EMAIL_UPDATED("Dear customer, your email was successfully changed!"),
+    ROLE_UPDATED("Dear customer, your role was successfully changed!"),
+    GENDER_UPDATED("Dear customer, your gender was successfully changed!"),
+    EMAIL_UPDATED_BY_ADMINISTRATION("Dear customer, your email was successfully changed by administration!"),
+    PASSWORD_UPDATED_BY_ADMINISTRATION("Dear customer, your password was successfully changed by administration!"),
+    NAME_UPDATED_BY_ADMINISTRATION("Dear customer, your name was successfully changed by administration!"),
+    ROLE_UPDATED_BY_ADMINISTRATION("Dear customer, your role was successfully changed by administration!");
+
+    private String value;
+
+    Notifications(String value) {
+        this.value = value;
+    }
+
+    public String getValue() {
+        return value;
+    }
+}
