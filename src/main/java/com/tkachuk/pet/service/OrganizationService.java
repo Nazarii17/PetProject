9f99fe97dd99624be88093a61f72af268491b2f4
Nazarii Tkachuk <nazar.tkachuk.28@gmail.com>
2020-03-29 22:05:38 +0300

    updated OrganizationService.java

--- src/main/java/com/tkachuk/pet/service/OrganizationService.java
+++ src/main/java/com/tkachuk/pet/service/OrganizationService.java
@@ -1,7 +1,7 @@
 package com.tkachuk.pet.service;
 
-import com.tkachuk.pet.dto.OrganizationCommonInfoDto;
 import com.tkachuk.pet.dto.OrganizationDto;
+import com.tkachuk.pet.dto.OrganizationProfileDto;
 import com.tkachuk.pet.entity.Organization;
 import com.tkachuk.pet.entity.OrganizationPhoto;
 import com.tkachuk.pet.entity.User;
@@ -10,11 +10,13 @@ import com.tkachuk.pet.repository.OrganizationRepo;
 import com.tkachuk.pet.util.FileUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Set;
 
@@ -22,8 +24,19 @@ import java.util.Set;
 public class OrganizationService {
 
     @Autowired
-    @Qualifier("basePath")
-    private String uploadPath;
+    @Qualifier("organizationLogoFilepath")
+    private String uploadLogoPath;
+
+    @Value("${filepath.photo.organization.logos}")
+    private String organizationLogoFilepath;
+
+    @Autowired
+    @Qualifier("organizationPhotosFilepath")
+    private String uploadPhotosPath;
+
+    @Value("${filepath.photo.organization.photos}")
+    private String organizationPhotosFilepath;
+
 
     private final OrganizationRepo organizationRepo;
     private final OrganizationMapper organizationMapper;
@@ -32,7 +45,9 @@ public class OrganizationService {
 
     @Autowired
     public OrganizationService(OrganizationRepo organizationRepo,
-                               OrganizationMapper organizationMapper, PhotoService photoService, UserService userService) {
+                               OrganizationMapper organizationMapper,
+                               PhotoService photoService,
+                               UserService userService) {
         this.organizationRepo = organizationRepo;
         this.organizationMapper = organizationMapper;
         this.photoService = photoService;
@@ -59,30 +74,34 @@ public class OrganizationService {
     }
 
     /**
-     * Sets given logo to a given organization;
+     * Changes logo of the organization with given id.
+     * Finds the organization by id.
+     * Validates a given file then saves the file;
+     * Sets the filename to an organization
+     *
+     * @param id   - id of the organization photo of which should be changed;
+     * @param file - file from ui to change on;
+     * @throws IOException - IOException
+     */
+    public void changeLogo(Long id,
+                           MultipartFile file) {
+        Organization organization = getOne(id);
+        saveLogo(organization, file);
+        save(organization);
+    }
+
+    /**
+     * Saves the given logo and sets to an organization, then returns an organization with saved logo;
+     * Sets a filename of a given logo to a given organization;
      *
      * @param organization - Organization on which should be putted logo;
      * @param file         - Given logo from user to set up to a given organization;
      * @throws IOException - IO error during saving the file;
      */
-    public void setLogo(Organization organization,
-                        MultipartFile file) throws IOException {
-
-        if (FileUtil.isFileValid(file)) {
-            String resultFilename = FileUtil.saveFile(uploadPath, file);
-            organization.setLogo(resultFilename);
-        }
-    }
-
-    public void changeLogo(Long id,
-                           MultipartFile file) throws IOException {
-
-        Organization organization = getOne(id);
-        if (FileUtil.isFileValid(file)) {
-            String resultFilename = FileUtil.saveFile(uploadPath, file);
-            organization.setLogo(resultFilename);
-        }
-        save(organization);
+    private Organization saveLogo(Organization organization, MultipartFile file) {
+        String fileName = FileUtil.savePhoto(uploadLogoPath, file);
+        organization.setLogo(fileName);
+        return organization;
     }
 
     /**
@@ -93,8 +112,52 @@ public class OrganizationService {
      * @param id - Given id from UI;
      * @return - OrganizationDto;
      */
-    public OrganizationDto findDtoById(Long id) {
-        return organizationMapper.toOrganizationDto(organizationRepo.getOne(id));
+    public OrganizationProfileDto findById(Long id) {
+        OrganizationProfileDto organization = organizationMapper.toOrganizationProfileDto(organizationRepo.getOne(id));
+        return overwriteFilePaths(organization);
+    }
+
+    public OrganizationProfileDto overwriteFilePaths(OrganizationProfileDto organizationProfileDto) {
+        organizationProfileDto = overwriteLogoPath(organizationLogoFilepath, organizationProfileDto);
+
+        Set<OrganizationPhoto> organizationPhotos = organizationProfileDto.getOrganizationPhotos();
+        organizationProfileDto.setOrganizationPhotos(overwritePhotoPaths(organizationPhotos, organizationPhotosFilepath));
+
+        return organizationProfileDto;
+    }
+
+    public OrganizationProfileDto overwriteLogoPath(String organizationLogoFilepath,
+                                                    OrganizationProfileDto organizationProfileDto) {
+        if (organizationProfileDto.getLogo() != null) {
+            organizationProfileDto.setLogo(organizationLogoFilepath + organizationProfileDto.getLogo());
+        }
+        return organizationProfileDto;
+    }
+
+    public Set<OrganizationPhoto> overwritePhotoPaths(Set<OrganizationPhoto> organizationPhotos,
+                                                      String organizationPhotosFilepath) {
+        organizationPhotos.stream()
+                .filter(organizationPhoto ->
+                        organizationPhoto.getName() != null)
+                .forEach(organizationPhoto ->
+                        organizationPhoto.setName((organizationPhotosFilepath + organizationPhoto.getName())));
+        return organizationPhotos;
+    }
+
+    public Set<OrganizationPhoto> findAllPhotosByOrganizationId(Long id) {
+        Set<OrganizationPhoto> organizationPhotos = getOne(id).getOrganizationPhotos();
+        overwritePhotoPaths(organizationPhotos, organizationPhotosFilepath);
+        return organizationPhotos;
+    }
+
+    public List<OrganizationDto> overwriteLogoPaths(String organizationLogoFilepath,
+                                                    List<OrganizationDto> organizations) {
+        organizations.stream()
+                .filter(organizationDto ->
+                        organizationDto.getLogo() != null)
+                .forEach(organizationDto ->
+                        organizationDto.setLogo(organizationLogoFilepath + organizationDto.getLogo()));
+        return organizations;
     }
 
     /**
@@ -104,8 +167,10 @@ public class OrganizationService {
      *
      * @return - List of all 'OrganizationCommonInfoDto';
      */
-    public List<OrganizationCommonInfoDto> findAllCommonInfoDto() {
-        return organizationMapper.toCommonInfoDtoList(organizationRepo.findAll());
+    public List<OrganizationDto> findAll() {
+        List<OrganizationDto> organizations = organizationMapper.toOrganizationDtoList(organizationRepo.findAll());
+
+        return overwriteLogoPaths(organizationLogoFilepath, organizations);
     }
 
     /**
@@ -113,35 +178,27 @@ public class OrganizationService {
      *
      * @param user            - user from current session;
      * @param organizationDto - value from UI form, made by user;
-     * @param file            - (image) logo of an organization;
-     * @throws IOException - error with file process;
+     * @return - saved Organization;
      */
-    public void save(User user,
-                     OrganizationDto organizationDto,
-                     MultipartFile file
-    ) throws IOException {
-        User userFromDb = userService.fromAuthenticationPrincipalToEntity(user);// TODO Is it ok?
-        Organization organization = organizationMapper.fromOrganizationDtoToEntity(organizationDto, userFromDb);// TODO Is it ok?
-
-        setLogo(organization, file);
-        save(organization);
+    public Organization save(User user, OrganizationDto organizationDto) {
+        User userFromDb = userService.getOne(user.getId());
+        Organization organization = organizationMapper.toEntity(organizationDto, userFromDb);
+        return save(organization);
     }
 
     /**
      * Updates an organization from DB;
      *
-     * @param id              - id of Organization(uses to find current organization in DB);
-     * @param user            - user from current session;
-     * @param organizationDto - value from UI form, made by user;
-     * @param file            - (image) logo of an organization;
-     * @throws IOException - error with file process;
+     * @param id                     - id of Organization(uses to find current organization in DB);
+     * @param user                   - user from current session;
+     * @param organizationProfileDto - value from UI form, made by user;
+     * @return - updated Organization;
      */
-    public void update(Long id,
-                       User user,
-                       OrganizationDto organizationDto,
-                       MultipartFile file) throws IOException {
+    public Organization update(Long id,
+                               User user,
+                               OrganizationProfileDto organizationProfileDto) {
 
-        Organization organization = organizationMapper.fromOrganizationDtoToEntity(organizationDto, user);
+        Organization organization = organizationMapper.toEntity(organizationProfileDto, user);
 
         Organization organizationFromDb = getOne(id);
         User userFromDb = userService.fromAuthenticationPrincipalToEntity(user);
@@ -149,40 +206,68 @@ public class OrganizationService {
         organizationFromDb.setName(organization.getName());
         organizationFromDb.setWebsite(organization.getWebsite());
         organizationFromDb.setAddress(organization.getAddress());
-        organizationFromDb.setAuthor(userFromDb);// TODO Is it ok?
+        organizationFromDb.setAuthor(userFromDb);
         organizationFromDb.setPhoneNumber(organization.getPhoneNumber());
         organizationFromDb.setRating(organization.getRating());
         organizationFromDb.setDescription(organization.getDescription());
-        setLogo(organizationFromDb, file);
         organizationFromDb.setOrganizationTypes(organization.getOrganizationTypes());
 
-        save(organizationFromDb);
+        return save(organizationFromDb);
     }
 
-    //Todo should it be here or PhotoService?
-
     /**
-     * Sets photos to an Organization ith a given Id;
+     * Sets photos to an Organization with a given Id;
      *
      * @param id     - id Of an Organization to work with;
      * @param photos - given by user from UI photos to be sett to an Organization;
      * @return - Updated Organization;
-     * @throws IOException - File error;
      */
     public Organization addNewPhotos(Long id,
-                                     MultipartFile[] photos) throws IOException {
+                                     MultipartFile[] photos) {
         Organization organization = getOne(id);
-        Set<OrganizationPhoto> organizationOrganizationPhotos = organization.getOrganizationPhotos();
-        for (MultipartFile newPhoto : photos) {
-            OrganizationPhoto organizationPhoto = new OrganizationPhoto();
-            if (FileUtil.isFileValid(newPhoto)) {
-                photoService.updatePhotoName(newPhoto, organizationPhoto);
-                organizationOrganizationPhotos.add(photoService.save(organizationPhoto));
-            }
-        }
-        organization.setOrganizationPhotos(organizationOrganizationPhotos);
+        Arrays.stream(photos).forEach(FileUtil::validateFile);
+        Set<OrganizationPhoto> organizationPhotos = organizationRepo.getOne(id).getOrganizationPhotos();
+        Arrays.stream(photos)
+                .map(this::createOrganizationPhoto)
+                .forEach(organizationPhotos::add);
+        organization.setOrganizationPhotos(organizationPhotos);
         return save(organization);
     }
 
+    /**
+     * Creates and saves an OrganizationPhoto;
+     *
+     * @param newPhoto - file to be converted to an OrganizationPhoto;
+     * @return saved OrganizationPhoto;
+     */
+    public OrganizationPhoto createOrganizationPhoto(MultipartFile newPhoto) {
+        OrganizationPhoto organizationPhoto = new OrganizationPhoto();
+        String resultFilename = FileUtil.savePhoto(uploadPhotosPath, newPhoto);
+        organizationPhoto.setName(resultFilename);
+        return photoService.save(organizationPhoto);
+    }
 
+    /**
+     * Updates photo with a given id to a given photo;
+     *
+     * @param id   - Id of photo which should be updated:
+     * @param file - photo which should change existed one;
+     * @return - saved OrganizationPhoto;
+     */
+    public OrganizationPhoto updatePhoto(Long id,
+                                         MultipartFile file) {
+        OrganizationPhoto organizationPhoto = photoService.getOne(id);
+        OrganizationPhoto photo = createOrganizationPhoto(file);
+        organizationPhoto.setName(photo.getName());
+        return photoService.save(organizationPhoto);
+    }
+
+    /**
+     * Deletes an Organization photo by Id;
+     *
+     * @param id - of photo which should be deleted;
+     */
+    public void deletePhoto(Long id) {
+        photoService.deletePhoto(id);
+    }
 }
