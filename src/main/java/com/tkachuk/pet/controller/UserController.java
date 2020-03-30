89d709d329f4bf1f1ce34ec074e9734901900e31
Nazarii Tkachuk <nazar.tkachuk.28@gmail.com>
2020-03-30 21:13:49 +0300

    updated profile.html, userProfileEdit.html, UserController.java

--- src/main/java/com/tkachuk/pet/controller/UserController.java
+++ src/main/java/com/tkachuk/pet/controller/UserController.java
@@ -1,10 +1,8 @@
 package com.tkachuk.pet.controller;
 
-import com.tkachuk.pet.dto.UserAdditionFormWithPasswordDto;
 import com.tkachuk.pet.dto.UserDto;
 import com.tkachuk.pet.dto.UserProfileDto;
 import com.tkachuk.pet.entity.Gender;
-import com.tkachuk.pet.entity.Role;
 import com.tkachuk.pet.entity.User;
 import com.tkachuk.pet.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,10 +15,9 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import javax.validation.Valid;
-import java.io.IOException;
 
 @Controller
-@RequestMapping("/user")
+@RequestMapping("/users")
 @PreAuthorize("hasAuthority('USER')")
 public class UserController {
 
@@ -31,50 +28,10 @@ public class UserController {
         this.userService = userService;
     }
 
-    @PreAuthorize("hasAuthority('ADMIN')")
-    @GetMapping("/all")
-    public String getUsersCommonInfoDtoList(Model model) {
-        model.addAttribute("usersCommonInfoDtoList", userService.findAllCommonInfoDto());
-        return "userList";
-    }
-
-    @PreAuthorize("hasAuthority('ADMIN')")
-    @GetMapping("/edit/{id}")
-    public String getUserEditForm(@PathVariable("id") Long id,
-                                  Model model) {
-        model.addAttribute("userAdditionFormWithPasswordDto", userService.getOneUserAdditionFormWithPasswordDtoById(id));
-        model.addAttribute("roles", Role.values());
-        return "editUser";
-    }
-
-    @PreAuthorize("hasAuthority('ADMIN')")
-    @PostMapping("/edit/{id}")
-    public String saveByAdministration(@PathVariable("id") Long id,
-                                       @Valid UserAdditionFormWithPasswordDto userAdditionFormWithPasswordDto,
-                                       BindingResult bindingResult,
-                                       Model model) {
-        if (bindingResult.hasErrors()) {
-            model.addAttribute("userAdditionFormWithPasswordDto", userAdditionFormWithPasswordDto);
-            model.addAttribute("roles", Role.values());
-            return "editUser";
-        } else {
-            userService.update(userAdditionFormWithPasswordDto, id);
-        }
-        return "redirect:/user/all";
-    }
-
-    @PreAuthorize("hasAuthority('ADMIN')")
-    @GetMapping("/delete/{id}")
-    public String delete(@PathVariable("id") long id) {
-        userService.deleteById(id);
-        return "redirect:/user/all";
-    }
-
     @GetMapping("/profile")
     public String getUserProfile(@AuthenticationPrincipal User user,
                                  Model model) {
-        model.addAttribute("userProfileDto", userService.getOneUserProfileDto(user.getId()));
-        model.addAttribute("userDto", userService.getOneUserDto(user.getId()));
+        model.addAttribute("user", userService.findById(user.getId()));
         return "profile";
     }
 
@@ -84,33 +41,26 @@ public class UserController {
                                  BindingResult bindingResult,
                                  Model model) {
         if (bindingResult.hasErrors()) {
-            model.addAttribute("userProfileDto", userService.getOneUserProfileDto(id));
+            model.addAttribute("user", userService.findById(id));
             return "profile";
         } else {
             userService.updateUsername(id, userDto);
-            return "redirect:/user/profile";
+            return "redirect:/users/profile";
         }
     }
 
     @PostMapping("/profile/change-photo/{id}")
     public String updateProfilePhoto(@PathVariable("id") Long id,
-                                     @RequestParam("file") MultipartFile photo) throws IOException {
-        userService.setProfilePhoto(id, photo);
-        return "redirect:/user/profile";
-    }
-
-    @GetMapping(value = "/find-by-name", params = {"wanted-name"})
-    public String findByUsernameStartsWith(@RequestParam(value = "wanted-name") String wantedName, Model model) {
-        model.addAttribute("usersCommonInfoDtoList", userService.findAllCommonInfoDtoByUsernameStartsWith(wantedName));
-        return "userList";
+                                     @RequestParam("file") MultipartFile photo) {
+        userService.changeProfilePhoto(id, photo);
+        return "redirect:/users/profile";
     }
 
     @GetMapping("/profile/edit/{id}")
     public String getUserProfileEditForm(@PathVariable("id") Long id,
-                                         @Valid UserProfileDto userProfileDto,
-                                         BindingResult bindingResult,
+                                         UserProfileDto userProfileDto,
                                          Model model) {
-        model.addAttribute("userProfileDto", userService.getOneUserProfileDto(id));
+        model.addAttribute("user", userService.findById(id));
         model.addAttribute("genders", Gender.values());
         return "userProfileEdit";
     }
@@ -121,13 +71,13 @@ public class UserController {
                                   BindingResult bindingResult,
                                   Model model) {
         if (bindingResult.hasErrors()) {
-            model.addAttribute("userProfileDto", userService.getOneUserProfileDto(id));
+            model.addAttribute("user", userService.findById(id));
             model.addAttribute("genders", Gender.values());
             return "userProfileEdit";
         } else {
             userService.update(userProfileDto, id);
         }
-        return "redirect:/user/profile";
+        return "redirect:/users/profile";
     }
 
 }
