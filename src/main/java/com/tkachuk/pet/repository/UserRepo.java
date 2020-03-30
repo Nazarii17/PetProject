d2bbf02f9366720dbc90a5e70b08dcda0ed2f1f4
Nazarii Tkachuk <nazar.tkachuk.28@gmail.com>
2020-03-30 23:20:30 +0300

    updated UserRepo.java

--- src/main/java/com/tkachuk/pet/repository/UserRepo.java
+++ src/main/java/com/tkachuk/pet/repository/UserRepo.java
@@ -3,12 +3,12 @@ package com.tkachuk.pet.repository;
 import com.tkachuk.pet.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.Collection;
+import java.util.List;
 
 public interface UserRepo extends JpaRepository<User, Long> {
     User findByUsername(String username);
 
     User findByActivationCode(String code);
 
-    Collection<User> findByUsernameStartsWith(String wantedName);
+    List<User> findByUsernameStartsWith(String wantedName);
 }
