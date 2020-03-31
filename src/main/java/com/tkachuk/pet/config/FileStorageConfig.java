f2d8f5de5cf13a77434b34cc18a01dffea9e2d6e
Nazarii Tkachuk <nazar.tkachuk.28@gmail.com>
2020-03-31 17:23:32 +0300

    updated updated UserUtil.java, UserService.java, Organization.java

--- src/main/java/com/tkachuk/pet/config/FileStorageConfig.java
+++ src/main/java/com/tkachuk/pet/config/FileStorageConfig.java
@@ -11,6 +11,13 @@ import java.io.File;
 @Slf4j
 public class FileStorageConfig {
 
+    private static final String DIR_USER = "users";
+    private static final String DIR_USER_PROFILE_PHOTO = "profile-photos";
+    private static final String DIR_USER_PHOTOS = "photos";
+    private static final String DIR_ORGANIZATION = "organizations";
+    private static final String DIR_ORGANIZATION_LOGO = "logos";
+    private static final String DIR_ORGANIZATION_PHOTOS = "photos";
+
     @Value("${upload.path}")
     private String uploadPath;
 
@@ -26,25 +33,26 @@ public class FileStorageConfig {
 
     @Bean(name = "uploadUserProfilePhotoFilepath")
     public String userProfilePhotoFilepath() {
-        File baseDir = getFile("users", "profile-photos");
+        File baseDir = getFile(DIR_USER, DIR_USER_PROFILE_PHOTO);
         return baseDir.getAbsolutePath();
     }
 
     @Bean(name = "uploadUserPhotosFilepath")
     public String userPhotosFilepath() {
-        File baseDir = getFile("users", "photos");
+
+        File baseDir = getFile(DIR_USER, DIR_USER_PHOTOS);
         return baseDir.getAbsolutePath();
     }
 
     @Bean(name = "uploadOrganizationLogoFilepath")
     public String organizationLogoFilepath() {
-        File baseDir = getFile("organizations", "logos");
+        File baseDir = getFile(DIR_ORGANIZATION, DIR_ORGANIZATION_LOGO);
         return baseDir.getAbsolutePath();
     }
 
     @Bean(name = "uploadOrganizationPhotosFilepath")
     public String organizationPhotosFilepath() {
-        File baseDir = getFile("organizations", "photos");
+        File baseDir = getFile(DIR_ORGANIZATION, DIR_ORGANIZATION_PHOTOS);
         return baseDir.getAbsolutePath();
     }
 
@@ -52,28 +60,28 @@ public class FileStorageConfig {
 
     @Bean(name = "filepathUserProfilePhoto")
     public String filepathUserProfilePhoto() {
-        return getUrl("users", "profile-photos");
+        return getUrl(DIR_USER, DIR_USER_PROFILE_PHOTO);
     }
 
     @Bean(name = "filepathUserPhotos")
     public String filepathUserPhotos() {
-        return getUrl("users", "photos");
+        return getUrl(DIR_USER, DIR_USER_PHOTOS);
     }
 
     @Bean(name = "filepathOrganizationLogo")
     public String filepathOrganizationLogo() {
-        return getUrl("organizations", "logos");
+        return getUrl(DIR_ORGANIZATION, DIR_ORGANIZATION_LOGO);
     }
 
     @Bean(name = "filepathOrganizationPhotos")
     public String filepathOrganizationPhotos() {
-        return getUrl("organizations", "photos");
+        return getUrl(DIR_ORGANIZATION, DIR_ORGANIZATION_PHOTOS);
     }
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
     public File getFile(String... folderNames) {
-        File baseDir = new File(getFilepath(folderNames));
+        File baseDir = new File(getUploadPath(folderNames));
         if (!baseDir.exists()) {
             baseDir.mkdirs();
             log.info(baseDir.getName() + " '" + baseDir.getAbsolutePath() + "' created");
@@ -81,19 +89,21 @@ public class FileStorageConfig {
         return baseDir;
     }
 
-    public String getFilepath(String... folderNames) {
-        StringBuilder filepath = new StringBuilder(File.separator);
-        for (String s : folderNames) {
-            filepath.append(s).append(File.separator);
-        }
+    public String getUploadPath(String... folderNames) {
+        StringBuilder filepath = getFilepath(folderNames);
         return uploadPath + filepath;
     }
 
     public String getUrl(String... folderNames) {
+        StringBuilder filepath = getFilepath(folderNames);
+        return File.separator + uploadPathRoot + File.separator + uploadPath + filepath;
+    }
+
+    public StringBuilder getFilepath(String[] folderNames) {
         StringBuilder filepath = new StringBuilder(File.separator);
         for (String s : folderNames) {
             filepath.append(s).append(File.separator);
         }
-        return File.separator + uploadPathRoot + File.separator + uploadPath + filepath;
+        return filepath;
     }
 }
