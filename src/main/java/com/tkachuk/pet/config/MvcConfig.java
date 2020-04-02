f5846f69d18efcd6ca8bdda661709bb09e8b4c82
Nazarii Tkachuk <nazar.tkachuk.28@gmail.com>
2020-04-02 14:25:02 +0300

    updated MvcConfig.java

--- src/main/java/com/tkachuk/pet/config/MvcConfig.java
+++ src/main/java/com/tkachuk/pet/config/MvcConfig.java
@@ -2,7 +2,6 @@ package com.tkachuk.pet.config;
 
 
 import org.modelmapper.ModelMapper;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
@@ -21,12 +20,6 @@ public class MvcConfig implements WebMvcConfigurer {
     public ModelMapper modelMapper() {
         return new ModelMapper();
     }
-
-    /**
-     * Takes a 'upload.path' from 'application.properties';
-     */
-    @Value("${upload.path}")
-    private String uploadPath;
 
     /**
      * Serving static resources from the classpath;
