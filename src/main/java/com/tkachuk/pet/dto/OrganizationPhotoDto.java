7531feafda595e71143bbb499af7631c9abe1471
Nazarii Tkachuk <nazar.tkachuk.28@gmail.com>
2020-04-06 13:03:57 +0300

    added OrganizationPhotoDto.java

---
+++ src/main/java/com/tkachuk/pet/dto/OrganizationPhotoDto.java
@@ -0,0 +1,12 @@
+package com.tkachuk.pet.dto;
+
+import lombok.*;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter @Setter @ToString
+public class OrganizationPhotoDto {
+
+    private Long id;
+    private String name;
+}
