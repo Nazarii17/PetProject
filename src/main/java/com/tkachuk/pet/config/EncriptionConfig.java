b91626709c128287253551c26eb25fe6bed662f3
Nazarii Tkachuk <nazar.tkachuk.28@gmail.com>
2020-04-06 12:51:05 +0300

    updated EncriptionConfig.java, WebSecurityConfig.java

--- src/main/java/com/tkachuk/pet/config/EncriptionConfig.java
+++ src/main/java/com/tkachuk/pet/config/EncriptionConfig.java
@@ -3,15 +3,13 @@ package com.tkachuk.pet.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.MessageDigestPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 public class EncriptionConfig {
     /**
-     * Encoding for MySQL
+     * Bean {@link PasswordEncoder} that uses in coding password.
      */
-
     @Bean
     public BCryptPasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
