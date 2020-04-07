c1f40f7e73adc0e0af68a934b9272088e13366bf
Nazarii Tkachuk <nazar.tkachuk.28@gmail.com>
2020-04-07 15:07:59 +0300

    updated OrganizationMapper.java

--- src/main/java/com/tkachuk/pet/mapper/OrganizationMapper.java
+++ src/main/java/com/tkachuk/pet/mapper/OrganizationMapper.java
@@ -1,36 +1,34 @@
 package com.tkachuk.pet.mapper;
 
 import com.tkachuk.pet.dto.OrganizationDto;
+import com.tkachuk.pet.dto.OrganizationPhotoDto;
 import com.tkachuk.pet.dto.OrganizationProfileDto;
 import com.tkachuk.pet.dto.UserDto;
 import com.tkachuk.pet.entity.Organization;
 import com.tkachuk.pet.entity.User;
+import lombok.AllArgsConstructor;
 import lombok.NonNull;
 import org.modelmapper.ModelMapper;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import java.util.Collection;
 import java.util.List;
+import java.util.Set;
 import java.util.stream.Collectors;
 
 @Component
+@AllArgsConstructor
 public class OrganizationMapper {
 
     private final ModelMapper mapper;
     private final UserMapper userMapper;
-
-    @Autowired
-    public OrganizationMapper(ModelMapper mapper, UserMapper userMapper) {
-        this.mapper = mapper;
-        this.userMapper = userMapper;
-    }
+    private final OrganizationPhotoMapper photoMapper;
 
     /**
-     * Converts a given Organization to a DTO;
+     * Converts a given {@link Organization} to a DTO;
      *
-     * @param organizations - organization to convert;
-     * @return - OrganizationLightDto with values of given organization;
+     * @param organizations - {@link Organization} to convert;
+     * @return - OrganizationLightDto with values of given {@link Organization};
      */
     public List<OrganizationDto> toOrganizationDtoList(@NonNull Collection<Organization> organizations) {
         return organizations.stream()
@@ -39,23 +37,25 @@ public class OrganizationMapper {
     }
 
     /**
-     * Converts a given Organization to a DTO;
+     * Converts a given {@link Organization} to a {@link OrganizationDto};
      *
-     * @param organization - organization to convert;
-     * @return - OrganizationDto with values of given organization;
+     * @param organization - {@link Organization} to convert;
+     * @return - {@link OrganizationDto} with values of given {@link Organization};
      */
     public OrganizationDto toOrganizationDto(Organization organization) {
         return mapper.map(organization, OrganizationDto.class);
     }
 
     /**
-     * Converts a given Organization to a DTO;
+     * Converts a given {@link Organization} to a {@link OrganizationProfileDto};
      *
-     * @param organization - organization to convert;
-     * @return - OrganizationProfileDto with values of given organization;
+     * @param organization - {@link Organization} to convert;
+     * @return - {@link OrganizationProfileDto} with values of given {@link Organization};
      */
     public OrganizationProfileDto toOrganizationProfileDto(Organization organization) {
         UserDto userDto = userMapper.toUserDto(organization.getAuthor());
+        Set<OrganizationPhotoDto> organizationPhotos = photoMapper.toDtoSet(organization.getOrganizationPhotos());
+
         return OrganizationProfileDto.builder()
                 .id(organization.getId())
                 .name(organization.getName())
@@ -67,16 +67,16 @@ public class OrganizationMapper {
                 .description(organization.getDescription())
                 .logo(organization.getLogo())
                 .organizationTypes(organization.getOrganizationTypes())
-                .organizationPhotos(organization.getOrganizationPhotos())
+                .organizationPhotos(organizationPhotos)
                 .build();
     }
 
     /**
-     * Converts given OrganizationDto to an Entity;
+     * Converts given any DTO of {@link Organization} to an Entity{@link Organization};
      *
      * @param f    - given DTO to convert to entity;
-     * @param user - User from session; Todo: is user from session?
-     * @return - New organization with values from given OrganizationDto;
+     * @param user - {@link User} from session;
+     * @return - New {@link Organization} with values from given OrganizationDto;
      */
     public <F> Organization toEntity(@NonNull F f, User user) {
         Organization organization = mapper.map(f, Organization.class);
@@ -84,6 +84,12 @@ public class OrganizationMapper {
         return organization;
     }
 
+    /**
+     * Converts from any DTO of {@link Organization} to Entity{@link Organization};
+     *
+     * @param <F> - any DTO of {@link Organization}
+     * @return - {@link Organization}
+     */
     public <F> Organization toEntity(@NonNull F f) {
         return mapper.map(f, Organization.class);
     }
