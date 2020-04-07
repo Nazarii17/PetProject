1813ce5f93a1369912126f6a86565769bf1ab1b1
Nazarii Tkachuk <nazar.tkachuk.28@gmail.com>
2020-04-07 11:20:52 +0300

    updated ErrorMessage.java

--- src/main/java/com/tkachuk/pet/constant/ErrorMessage.java
+++ src/main/java/com/tkachuk/pet/constant/ErrorMessage.java
@@ -3,8 +3,12 @@ package com.tkachuk.pet.constant;
 public class ErrorMessage {
     public static final String USER_NOT_FOUND_BY_ID = "The user does not exist by this id: ";
     public static final String ORGANIZATION_NOT_FOUND_BY_ID = "The organization does not exist by this id: ";
+    public static final String ORGANIZATION_PHOTO_NOT_FOUND_BY_ID = "The organization photo does not exist by this id: ";
+
     public static final String ORGANIZATION_NOT_SAVED = "Organization wasn't saved ";
+    public static final String ORGANIZATION_PHOTO_NOT_SAVED = "Organization photo wasn't saved ";
     public static final String FILE_NOT_SAVED = "File wasn't saved ";
+
     public static final String FILE_IS_EMPTY = "File is empty ";
     public static final String FILE_NOT_VALID = "File is not valid ";
     public static final String FILE_NOT_IMAGE = "File is not an image ";
